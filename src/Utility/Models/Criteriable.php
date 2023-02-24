0c0fd2c831cbcef033849fec6500c3b30f669ef6
shamaseen <m.shamaseen@outlook.com>
2023-02-24 23:31:43 +0300

    append and set each of the criteria array

--- src/Utility/Models/Criteriable.php
+++ src/Utility/Models/Criteriable.php
@@ -8,24 +8,23 @@ use Illuminate\Database\Eloquent\Builder;
  * @method static Builder filterByCriteria(array $criteria)
  * @method static Builder searchByCriteria(array $criteria)
  * @method static Builder orderByCriteria(array $criteria)
+ * @method static Builder setSearchables(array $searchables)
+ * @method static Builder appendSearchables(array $searchables)
+ * @method static Builder setFilterables(array $filterables)
+ * @method static Builder appendFilterables(array $filterables)
+ * @method static Builder setSortables(array $sortables)
+ * @method static Builder appendSortables(array $sortables)
  */
 trait Criteriable
 {
     protected ?array $searchables = null;
-    protected ?array $filterable = null;
+    protected ?array $filterables = null;
     protected ?array $sortables = null;
 
     /**
      * @var array<array<string>>
      */
     protected ?array $fulltextSearch = [];
-
-    public function scopeAppendSortables($query, array $sortables): Builder
-    {
-        $this->sortables = collect($this->sortables)->merge($sortables)->unique()->toArray();
-
-        return $query;
-    }
 
     public function scopeFilterByCriteria($query, array $criteria): Builder
     {
@@ -106,6 +105,20 @@ trait Criteriable
         return array_diff($this->getFillable(), $this->getHidden());
     }
 
+    public function scopeAppendSearchables($query, array $searchables): Builder
+    {
+        $this->searchables = collect($this->searchables)->merge($searchables)->unique()->toArray();
+
+        return $query;
+    }
+
+    public function scopeSetSearchables($query, array $searchables): Builder
+    {
+        $this->searchables = $searchables;
+
+        return $query;
+    }
+
     /**
      * By default, all fillables and not hidden are sortables, if you want to override that explicitly set an array of sortables.
      */
@@ -118,11 +131,18 @@ trait Criteriable
         return array_diff($this->getFillable(), $this->getHidden());
     }
 
-    public function setSearchables($searchables): self
+    public function scopeAppendSortables($query, array $sortables): Builder
     {
-        $this->searchables = $searchables;
+        $this->sortables = collect($this->sortables)->merge($sortables)->unique()->toArray();
 
-        return $this;
+        return $query;
+    }
+
+    public function scopeSetSortables($query, array $sortables): Builder
+    {
+        $this->sortables = $sortables;
+
+        return $query;
     }
 
     /**
@@ -131,17 +151,24 @@ trait Criteriable
      */
     public function getFilterables(): array
     {
-        if (null !== $this->filterable) {
-            return $this->filterable;
+        if (null !== $this->filterables) {
+            return $this->filterables;
         }
 
         return array_diff($this->getFillable(), $this->getHidden());
     }
 
-    public function setFilterables($searchables): self
+    public function scopeAppendFilterables($query, array $filterables): Builder
     {
-        $this->searchables = $searchables;
+        $this->filterables = collect($this->filterables)->merge($filterables)->unique()->toArray();
 
-        return $this;
+        return $query;
+    }
+
+    public function scopeSetFilterables($query, array $filterables): Builder
+    {
+        $this->filterables = $filterables;
+
+        return $query;
     }
 }
